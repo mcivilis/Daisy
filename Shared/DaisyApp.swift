5b5314c6a1cf5acc03de389f13a9b95616de663b
Maria Civilis <maria.civilis@textnow.com>
2022-07-15 15:25:02 -0400

    Initial Commit

---
+++ Shared/DaisyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaisyApp.swift
+//  Shared
+//
+//  Created by Maria Civilis on 2022-07-15.
+//
+
+import SwiftUI
+
+@main
+struct DaisyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
