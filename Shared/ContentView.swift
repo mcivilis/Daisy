8bceccedacb403aae9adf446686500f65ec24268
Maria Civilis <maria.civilis@textnow.com>
2022-07-15 17:04:45 -0400

    list

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    var daisies: [Daisy]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(daisies, id: \.id) {
+                    Text("\($0.title)")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(daisies: Daisy.previewMany)
     }
 }
--- Shared/DaisyApp.swift
+++ Shared/DaisyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DaisyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(daisies: Daisy.previewMany)
         }
     }
 }
